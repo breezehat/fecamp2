67b2cae02c69ae69d104cb98f8ee2d90dfdb224b
breezehat <breezehat@gmail.com>
2015-03-12 19:54:47 +0900

    update

--- html/default.js
+++ html/default.js
@@ -1,5 +1,21 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
-		$(".gnb>ul").slideToggle(200);
+	//모바일 GNB 
+	var $gnbBtn = $(".gnb>button");
+	
+	$gnbBtn.on("click", function(){
+		$(this).next().slideToggle(200);
+
+		if($gnbBtn.text() === ("메뉴 열기")){
+			//메뉴 닫기 텍스트 표시
+			$gnbBtn.text("메뉴 닫기")
+		} else{
+			//메뉴 열기 텍스트 표시
+			$gnbBtn.text("메뉴 열기")
+		}
+	});
+
+	//태블릿+데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
 	});
 });
